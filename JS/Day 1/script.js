7cbafb46858bd9a25113a711b46ea0dd28671f68
Sarbin Tharu <sarbintharu852@gmail.com>
2026-08-07 09:00:14 +0545

    Initial commit

---
+++ JS/Day 1/script.js
@@ -0,0 +1,36 @@
+// let fname = 'Hari'
+// let lname = "Bahadur"
+
+// console.log(fname + lname)
+// console.log("First name is "+fname+" lastname is "+lname+"")
+
+//case
+
+// Snakecase = underscore
+
+// let first_name = "hari"
+
+// // pascalcase = first letter of each word is capitalized
+
+// let firstName ='hari'
+
+// //camel case
+
+// let firstNameAndLastName = 'hari'
+
+// // kebab case = hyphen
+
+// // let first-name = 'hari'
+
+// let lastName
+// lastName = 'oli'
+// console.log(lastName)
+
+// const country
+// country = 'Nepal'
+// console.log(country)
+
+let x = 5
+let y = 10
+
+console.log("Hello" + (x+y))
---
+++ JS/Day 2/script.js
@@ -0,0 +1,50 @@
+// console.log('Hello')
+
+// Loop
+// for(initialization; condition; change in variable){
+//     // statement
+// }
+
+// let x = 5
+// console.log(x++)
+// console.log(x)
+// let x
+// for(x = 0; x < 5; x++){
+//     console.log(x)
+
+// }
+
+// let x = 5
+// for(let y=1; y<=10; y++){
+//     console.log(x + "x"+y+ "=" +x*y)
+// }
+
+// While(condition){ statement }
+
+// let x = 1
+// while (x<6){
+//     console.log(x)
+//     x++
+// }
+
+// do{statement} while(condition)
+
+// let x = 1
+// do{
+//     console.log(x)
+//     x++
+// }
+// while(x<6)
+
+// variable declare, print "HELLO" as many as variable size
+
+// let x = 12
+// for(i=0; i<x ; i++){
+//     console.log("HELLO")
+// }
+
+let y = 0
+while(y<x){
+    console.log('hello')
+    y++
+}
