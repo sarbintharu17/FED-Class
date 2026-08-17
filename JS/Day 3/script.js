83b30b5ec2e648d3cb807cf5304f79b722733622
Sarbin Tharu <sarbintharu852@gmail.com>
2026-08-17 08:30:16 +0545

    updated

---
+++ JS/Day 3/script.js
@@ -0,0 +1,53 @@
+// console.log('First')
+// function myFunction(){
+//     console.log("hello function")
+// }
+
+// myFunction()
+
+// // int, void, string
+
+// function giveValue(){
+//     return 5
+// }
+
+// let x = giveValue()
+// console.log(x)
+
+
+// Parameters
+
+// function sum(a, b){
+//     console.log("sum" + (a+b))
+
+// }
+
+// // Arguments
+// sum(3,5)
+
+// Default parameters
+
+// function printNumber( a = 5){
+//     console.log("a is " + a)
+
+// }
+
+// printNumber(10)
+
+
+// create a function that takes two 
+// numbered parameters
+// create a loop inside function
+// and print all the numbers
+// from first parameter to
+// second
+
+function parameters(a,b)
+{
+
+    for( let i = a; i<=b; i++){
+        console.log(i)
+    }
+}
+
+parameters(1,100)
