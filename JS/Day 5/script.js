144599617b88410ffc55f686ff1bb7a6ba752b1e
Sarbin Tharu <sarbintharu852@gmail.com>
2026-08-24 19:50:49 +0545

    update

---
+++ JS/Day 5/script.js
@@ -0,0 +1,25 @@
+// const Marks = [
+//     {name: "Ram", result: [60,75,89]},
+//     {name: "Hari", result: [78,48,98]}
+// ]
+
+// for( let i = 0; i<Marks.length; i++)
+// {
+//     console.log(Marks[i].name + "got" + Marks[i].result)
+// }
+
+
+  const Account = [
+    {acc_no: 12345, name: "Ram", Balance: 50000},
+    {acc_no:78906, name: "Hari", Balance: 60000}
+  ]
+
+
+  function deposit(acc_no, amount)
+  { 
+    if(amount>0)
+    {   balance = balance + amount
+        console.log(deposited{amount} to {ac)
+    }
+    
+  }
